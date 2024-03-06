1553b20047e33ed512e4d83f818291f34f7bc2db
Sreenaina Koujala <72166645+Nainicorn@users.noreply.github.com>
2024-03-06 11:22:55 -0500

    Update aws.js to use shorted api uri

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,7 +1,7 @@
 let page = location.hostname;
-
+let apigateway = 'https://mdapi.sreenaina.com/api'; //or https://masondashapi.sreenaina.com/api
 const aws = { 
-    uri: (page === 'localhost') ? '/api': 'https://masondashapi.sreenaina.com/api'
+    uri: (page === 'localhost') ? '/api': apigateway
 };
 
 export default aws;
