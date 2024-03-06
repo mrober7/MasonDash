b0ced5761e969e318cf87f64b13d1d739febcaf2
Sreenaina Koujala <72166645+Nainicorn@users.noreply.github.com>
2024-03-06 11:47:10 -0500

    Update aws.js

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,5 +1,7 @@
+let page = location.hostname;
+let apigateway = 'https://mdapi.sreenaina.com/api'; //or https://masondashapi.sreenaina.com/api
 const aws = { 
-    uri: 'https://mdapi.sreenaina.com/api' //or https://masondashapi.sreenaina.com/api
+    uri: (page === 'localhost') ? '/api': apigateway
 };
 
 export default aws;
