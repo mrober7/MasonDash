af4c651959b409c03b02e449ba94706460174592
nainicorn <nkoujala@gmail.com>
2024-03-25 22:11:58 -0400

    Updates for credits

--- src/components/credits/credits.js
+++ src/components/credits/credits.js
@@ -1,16 +1,18 @@
 // info.js file that handles functionality for the components that use info data
 // import html from info.hbs to display custom data in using innerHTML
-import template from './credits.hbs';
+import template from "./credits.hbs";
 // import info scss file to style and display the info on the dashboard
-import './credits.scss';
+import "./credits.scss";
 // import studentsService that retrieves students data
-import studentsService from 'services/studentsService';
+import studentsService from "services/studentsService";
+// import coursesService that retrieves courses data
+import coursesService from "services/coursesService";
 
 const credits = {
     // asynchronous initialization function for the info object
     async init() {
         // select the DOM element with the class item info
-        this.element = document.querySelector('.item.credits');
+        this.element = document.querySelector(".item.credits");
         // render the layout of the info component
         this._renderLayout();
         // load data async
@@ -25,18 +27,44 @@ const credits = {
         });
 
         // set the inner HTML of the selected element to the generated HTML
-        this.element.querySelector('.item-body').innerHTML = mainHtml;
+        this.element.querySelector(".item-body").innerHTML = mainHtml;
+    },
+
+    // private method to load data asynchronously for the courses component
+    async _getCourses(courseType) {
+        // fetch the list of all courses
+        this.coursesData = await coursesService.get();
+        // fetch information about the current student
+        let studentInfo = await studentsService.getMe();
+        // initialize an array to store matched courses
+        let matchedCourses = [];
+        // loop through the courses of the current student
+        studentInfo[courseType].forEach((courseId) => {
+            // filter the courses to find a match based on the course ID
+            let match = this.coursesData.filter((course) => {
+                return course.id === courseId;
+            });
+
+            // add the matched courses to the array
+            matchedCourses.push(...match);
+        });
     },
 
     // private method to load data asynchronously for the info component
     async _loadData() {
         // fetch information about the current student info to display in pills
-        let studentInfo = await studentsService.getMe();
+        let courses = await this._getCourses('courses');
+        let finishedcourses = await this._getCourses('finishedcourses');
+        let futurecourses = await this._getCourses('futurecourses');
         let pillHtml = template({
             pill: true,
-            data: studentInfo
+            courses,
+            finishedcourses,
+            futurecourses
         });
-        this.element.querySelector(`.pills`).insertAdjacentHTML('beforeend', pillHtml);
+        this.element
+            .querySelector(`.pills`)
+            .insertAdjacentHTML("beforeend", pillHtml);
     },
 };
 // export info object to make it available for other modules to use
